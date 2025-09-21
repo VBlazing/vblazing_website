dcfc2a2aeb719591ed5275b570ba6e73624638a9
vblazing <18392192892@163.com>
2025-09-21 20:01:14 +0800

    import homeheroskeleton

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@
  * @Author: vblazing
  * @Date: 2025-09-02 18:01:24
  * @LastEditors: vblazing
- * @LastEditTime: 2025-09-20 18:50:15
+ * @LastEditTime: 2025-09-21 19:20:05
  * @Description:
  */
+import { Suspense } from "react";
 import HeroSection from "@/components/home/hero_section";
+import { HomeHeroSkeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
       {/* 介绍 */}
-      <HeroSection />
+      <Suspense fallback={<HomeHeroSkeleton />}>
+        <HeroSection />
+      </Suspense>
     </div>
   );
 }
