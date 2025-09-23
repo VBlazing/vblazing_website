441ffdfb7c6031b80b05c3c93452027fff6abf30
vblazing <18392192892@163.com>
2025-09-23 18:34:17 +0800

    feat: blog card

---
+++ components/common/blog_card.tsx
@@ -0,0 +1,94 @@
+/*
+ * @Author: vblazing
+ * @Date: 2025-09-23 15:52:28
+ * @LastEditors: vblazing
+ * @LastEditTime: 2025-09-23 18:32:54
+ * @Description: 博客卡片组件
+ */
+import Link from "next/link";
+import CategoryTag from "@/components/common/category_tag";
+import FeaturedImage from "@/components/common/featured_image";
+import BlogDate from "@/components/common/blog_date";
+import ReadingTime from "@/components/common/reading_time";
+import LabelList from "./label_list";
+import { getTranslations } from "next-intl/server";
+import { ArrowRight } from "lucide-react";
+
+// todo: 类型待修改
+interface IBlogCardPros {
+  blogInfo: {
+    id: string;
+    title: string;
+    content: string;
+    category: string;
+    lastEditedTime: string;
+    readingTime: number;
+    image: string;
+    labelList: {
+      key: string;
+      name: string;
+    }[];
+  };
+  showLabel?: boolean;
+}
+
+export default async function BlogCard({
+  blogInfo,
+  showLabel = false,
+}: IBlogCardPros) {
+  const t = await getTranslations("common");
+
+  const {
+    id,
+    title,
+    content,
+    category,
+    lastEditedTime,
+    readingTime,
+    image,
+    labelList,
+  } = blogInfo ?? {};
+  return (
+    <div className="bg-background group w-full overflow-hidden rounded-2xl border shadow-sm transition-all duration-300 hover:shadow-lg">
+      <div className="flex flex-col gap-0 lg:flex-row">
+        {/* 图片 */}
+        <div className="relative h-50 w-full overflow-hidden sm:h-64 lg:h-auto">
+          <Link href={`/detail/${id}`}>
+            <CategoryTag
+              category={category}
+              className="absolute top-3 left-3 z-50"
+            />
+            <FeaturedImage
+              url={image}
+              title={title}
+              className="group-hover:scale-105"
+            />
+          </Link>
+        </div>
+        {/* 博客内容 */}
+        <div className="w-full p-6 sm:p-8">
+          <div className="mb-6 flex w-full flex-col justify-center">
+            <div className="mb-4 flex items-center space-x-3 text-xs sm:space-x-5 sm:text-sm [&_svg]:size-3 sm:[&_svg]:size-4">
+              <BlogDate date={lastEditedTime} />
+              <ReadingTime time={readingTime} />
+            </div>
+            <h3 className="text-main-title mb-2 text-2xl leading-tight font-light sm:mb-4 lg:text-3xl">
+              <Link href={`/detail/${id}`}>{title}</Link>
+            </h3>
+            <p className="text-main-text mb-4 line-clamp-3 text-base leading-relaxed overflow-ellipsis lg:text-lg">
+              {content}
+            </p>
+            {showLabel && <LabelList labelList={labelList} className="mb-4" />}
+          </div>
+          <Link
+            href={`/detail/${id}`}
+            className="text-main-title inline-flex items-center space-x-2 font-medium transition-all duration-200 group-hover:translate-x-1"
+          >
+            <span>{t("read_more")}</span>
+            <ArrowRight className="size-3" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
