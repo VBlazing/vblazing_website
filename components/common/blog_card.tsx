ecbefc05b4b6933e6d6fe51d8047f649bf68ecb4
vblazing <18392192892@163.com>
2025-09-25 18:46:20 +0800

    feat: blog_card add class

--- components/common/blog_card.tsx
+++ components/common/blog_card.tsx
@@ -2,17 +2,18 @@
  * @Author: vblazing
  * @Date: 2025-09-23 15:52:28
  * @LastEditors: vblazing
- * @LastEditTime: 2025-09-25 17:03:42
+ * @LastEditTime: 2025-09-25 18:44:26
  * @Description: 博客卡片组件
  */
 import Link from "next/link";
+import { getTranslations } from "next-intl/server";
+import { ArrowRight } from "lucide-react";
 import CategoryTag from "@/components/common/category_tag";
 import FeaturedImage from "@/components/common/featured_image";
 import BlogDate from "@/components/common/blog_date";
 import ReadingTime from "@/components/common/reading_time";
-import LabelList from "./label_list";
-import { getTranslations } from "next-intl/server";
-import { ArrowRight } from "lucide-react";
+import LabelList from "@/components/common/label_list";
+import { cn } from "@/lib/utils";
 
 // todo: 类型待修改
 interface IBlogCardPros {
@@ -30,11 +31,13 @@ interface IBlogCardPros {
     }[];
   };
   showLabel?: boolean;
+  className?: string;
 }
 
 export default async function BlogCard({
   blogInfo,
   showLabel = false,
+  className,
 }: IBlogCardPros) {
   const t = await getTranslations("common");
 
@@ -49,7 +52,12 @@ export default async function BlogCard({
     labelList,
   } = blogInfo ?? {};
   return (
-    <div className="bg-background group @container flex w-full overflow-hidden rounded-2xl border shadow-sm transition-all duration-300 hover:shadow-lg">
+    <div
+      className={cn(
+        "bg-background group @container flex w-full overflow-hidden rounded-2xl border shadow-sm transition-all duration-300 hover:shadow-lg",
+        className,
+      )}
+    >
       <div className="flex flex-grow flex-col gap-0 @lg:flex-row">
         {/* 图片 */}
         <div className="relative h-50 w-full overflow-hidden @sm:h-64 @lg:h-auto">
