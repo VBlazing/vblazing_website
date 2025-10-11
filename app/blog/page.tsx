8ac412a96b5b3d4ed32b0c0c4054a8262b552f5b
vblazing <18392192892@163.com>
2025-10-11 12:29:41 +0800

    feat: add blog and product initial page

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+/*
+ * @Author: vblazing
+ * @Date: 2025-09-20 17:54:37
+ * @LastEditors: vblazing
+ * @LastEditTime: 2025-09-20 17:55:09
+ * @Description: 博客页面
+ */
+export default function Blog() {
+  return <div>this is blog</div>;
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,10 @@
+/*
+ * @Author: vblazing
+ * @Date: 2025-09-20 17:55:39
+ * @LastEditors: vblazing
+ * @LastEditTime: 2025-09-20 17:56:12
+ * @Description: 产品页面
+ */
+export default function Products() {
+  return <div>this is products</div>;
+}
