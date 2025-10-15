0b1f23e15e9fe2ef4d1d039fd1c54c85efac54a6
vblazing <18392192892@163.com>
2025-10-15 18:24:56 +0800

    feat: category filter

---
+++ components/common/category_filter.tsx
@@ -0,0 +1,82 @@
+/*
+ * @Author: vblazing
+ * @Date: 2025-10-15 14:09:32
+ * @LastEditors: vblazing
+ * @LastEditTime: 2025-10-15 18:21:11
+ * @Description: 文章分类筛选器
+ */
+"use client";
+
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { Button } from "@/components/ui/button";
+import { CategoryInfo } from "@/lib/definitions";
+import { cn } from "@/lib/utils";
+import { ALL } from "@/lib/const";
+
+interface ICategoryFilterProps {
+  category_list: CategoryInfo[];
+}
+
+export default function CategoryFilter({
+  category_list,
+}: ICategoryFilterProps) {
+  const t = useTranslations("common");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const categoryListWithAll = [
+    {
+      id: ALL,
+      name: t("all"),
+    },
+    ...category_list,
+  ];
+
+  const [value, setValue] = useState<string>(
+    searchParams.get("category") || "",
+  );
+
+  const createQuery = useDebouncedCallback((name: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value && value !== "-1") {
+      params.set(name, value);
+    } else {
+      params.delete(name);
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  const handleChange = (value: string) => {
+    setValue(value);
+    createQuery("category", value);
+  };
+
+  return (
+    <div className="border-border bg-background inline-flex items-center rounded-sm border-1 shadow-sm">
+      {categoryListWithAll.map((item) => {
+        const idStr = String(item.id);
+        const isChoose = idStr === value;
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => handleChange(idStr)}
+            key={item.id}
+            className={cn(
+              "hover:bg-background dark:hover:bg-background cursor-pointer rounded-none px-6 first:rounded-l-sm last:rounded-r-sm",
+              {
+                "bg-active-background text-active-text dark:hover:bg-active-background hover:bg-active-background dark:hover:text-active-text hover:text-active-text":
+                  isChoose,
+              },
+            )}
+          >
+            {item.name}
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
--- lib/const.ts
+++ lib/const.ts
@@ -1,4 +1,14 @@
+/*
+ * @Author: vblazing
+ * @Date: 2025-10-09 16:31:05
+ * @LastEditors: vblazing
+ * @LastEditTime: 2025-10-15 16:48:37
+ * @Description: 常量
+ */
 import { BlogState, BlogStateKey, BlogCategory, BlogCategoryKey } from "@/lib/definitions";
+
+// `所有`的id
+export const ALL = '-1'
 
 // 博客状态
 export const BLOG_STATE: Record<BlogStateKey, BlogState> = {
