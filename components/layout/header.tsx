827469b822438eb0ae968e0a6ceb685e285974b9
vblazing <18392192892@163.com>
2025-10-22 19:47:46 +0800

    debug navigation link

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,7 +2,7 @@
  * @Author: vblazing
  * @Date: 2025-09-05 21:32:17
  * @LastEditors: vblazing
- * @LastEditTime: 2025-10-22 19:40:43
+ * @LastEditTime: 2025-10-22 19:45:22
  * @Description: 页面顶部
  */
 import Link from "next/link";
@@ -43,19 +43,18 @@ export default async function Header() {
               console.log("menu item: ", item);
               return (
                 <NavigationMenuItem key={item.key}>
-                  <div>
-                    {item.title}
-                    <Link
-                      href={item.href}
-                      className="flex flex-row items-center"
-                    >
-                      <Icon className="text-foreground mr-1 h-4 w-4" />
-                      <span>{item.title}</span>
-                    </Link>
-                  </div>
-                  {/* <NavigationMenuLink>
-                    {item.title}
-                  </NavigationMenuLink> */}
+                  <NavigationMenuLink asChild>
+                    <div>
+                      {item.title}
+                      <Link
+                        href={item.href}
+                        className="flex flex-row items-center"
+                      >
+                        <Icon className="text-foreground mr-1 h-4 w-4" />
+                        <span>{item.title}</span>
+                      </Link>
+                    </div>
+                  </NavigationMenuLink>
                 </NavigationMenuItem>
               );
             })}
