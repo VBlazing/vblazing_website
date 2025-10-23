c6baceda8e06263b66ccf5b447f36fa1e800936a
vblazing <18392192892@163.com>
2025-10-23 12:49:14 +0800

    loading prefetch

--- app/[id]/detail/loading.tsx
+++ app/[id]/detail/loading.tsx
@@ -2,12 +2,14 @@
  * @Author: vblazing
  * @Date: 2025-10-11 15:18:50
  * @LastEditors: vblazing
- * @LastEditTime: 2025-10-11 15:25:38
+ * @LastEditTime: 2025-10-23 12:48:58
  * @Description: 详情加载中
  */
 import { getTranslations } from "next-intl/server";
 
 export default async function LoadingDetail() {
+  console.log("prefetch detail loading");
+
   const t = await getTranslations("detail");
   return (
     <div className="my-auto w-full text-center">
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,14 @@
+/*
+ * @Author: vblazing
+ * @Date: 2025-10-19 14:26:21
+ * @LastEditors: vblazing
+ * @LastEditTime: 2025-10-23 12:48:16
+ * @Description: loading 页面
+ */
 import { getTranslations } from "next-intl/server";
 
 export default async function LoadingDetail() {
+  console.log("prefetch loading");
   const t = await getTranslations("common");
   return (
     <div className="my-auto w-full text-center">
