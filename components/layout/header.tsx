4e10be9b1f2ca97f30c187a5f4e9c5420a38ec57
VBlazing <18392192892@163.com>
2025-10-23 21:31:51 +0800

    rollback

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,7 +2,7 @@
  * @Author: vblazing
  * @Date: 2025-09-05 21:32:17
  * @LastEditors: VBlazing
- * @LastEditTime: 2025-10-23 21:00:36
+ * @LastEditTime: 2025-10-23 21:25:12
  * @Description: 页面顶部
  */
 import Link from "next/link";
@@ -41,12 +41,13 @@ export default async function Header() {
               }
               return (
                 <NavigationMenuItem key={item.key}>
-                  <NavigationMenuLink asChild>
-                    <Link href={item.href} className="flex-row items-center">
-                      <Icon className="text-foreground mr-1 h-4 w-4" />
-                      <span>{item.title}</span>
-                    </Link>
-                  </NavigationMenuLink>
+                  <Link
+                    href={item.href}
+                    className="data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus-visible:ring-ring/50 [&_svg:not([class*='text-'])]:text-muted-foreground flex flex-row items-center gap-1 rounded-sm p-2 text-sm transition-all outline-none focus-visible:ring-[3px] focus-visible:outline-1 [&_svg:not([class*='size-'])]:size-4"
+                  >
+                    <Icon className="text-foreground mr-1 h-4 w-4" />
+                    <span>{item.title}</span>
+                  </Link>
                 </NavigationMenuItem>
               );
             })}
