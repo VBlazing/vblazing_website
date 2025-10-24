cc56ba10616df4ef39a9c1803cfdc2201fc593d8
VBlazing <18392192892@163.com>
2025-10-24 22:59:23 +0800

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,15 @@
 /*
  * @Author: VBlazing
- * @Date: 2025-10-24 20:34:36
+ * @Date: 2025-10-24 22:54:58
  * @LastEditors: VBlazing
- * @LastEditTime: 2025-10-24 20:34:50
- * @Description: i18n 代理
+ * @LastEditTime: 2025-10-24 22:57:13
+ * @Description: middleware
  */
-
 import createMiddleware from 'next-intl/middleware';
 import { routing } from '@/lib/i18n/routing';
 
 export default createMiddleware(routing);
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
