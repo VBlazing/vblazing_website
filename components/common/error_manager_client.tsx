e3c035a01f13e43e173fdda352e88e38bf07046e
VBlazing <18392192892@163.com>
2025-11-05 11:19:39 +0800

    fix: error catch

--- components/common/error_manager_client.tsx
+++ components/common/error_manager_client.tsx
@@ -1,11 +1,20 @@
+/*
+ * @Author: VBlazing
+ * @Date: 2025-10-14 11:08:28
+ * @LastEditors: VBlazing
+ * @LastEditTime: 2025-11-05 10:56:05
+ * @Description: 错误注入
+ */
 "use client";
 
 import { useEffect } from "react";
 import { initErrorManager } from "@/lib/error";
+import { useTranslations } from "next-intl";
 
 export default function ErrorManagerClient() {
+  const t = useTranslations("error");
   useEffect(() => {
-    initErrorManager();
-  }, []);
+    initErrorManager(t);
+  }, [t]);
   return null;
 }
